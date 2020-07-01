20044f0e3e87b9026c6986c4a18b0b0a0fa0edaf
nate-luup <nate.xu@everbridge.com>
2020-07-01 22:11:09 +0800

    doc(README): 开发环境搭建

--- js/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-console.log('hello word!');
-var md5 = function (key, value) {
-    return key + value;
-};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-console.log('hello word!')
+console.log("hello word!");
 
-interface encrypt{
-    (key: string, value: string):string;
-    (name: number, age: number):string;
+interface encrypt {
+  (key: string, value: string): string;
+  (name: number, age: number): string;
 }
 
-const md5: encrypt = (key: string, value:string)=>{
-    return key + value;
-}
+let num: number = 123;
