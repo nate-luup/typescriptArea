1c5112ecc1d450837fa9afe95739ae885a85f614
nate <nate.luup@gmail.com>
2022-05-01 17:50:18 +0800

    feat(00011): easy tuple to object

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,13 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [P in T[number]]: P
+}
+
+//js
+
+function tupleToObject(array) {
+  let result = {}
+  array.array.forEach((element) => {
+    result[element] = element
+  })
+  return result
+}
--- questions/00011-easy-tuple-to-object/test-cases.ts
+++ questions/00011-easy-tuple-to-object/test-cases.ts
@@ -1,10 +1,33 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
 const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+type r = typeof tuple
 
 type cases = [
-  Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y' }>>,
+  Expect<
+    Equal<
+      TupleToObject<typeof tuple>,
+      {
+        tesla: 'tesla'
+        'model 3': 'model 3'
+        'model X': 'model X'
+        'model Y': 'model Y'
+      }
+    >
+  >
 ]
 
 // @ts-expect-error
 type error = TupleToObject<[[1, 2], {}]>
+
+// const let js 世界
+// type interface 类型世界
+// 1. typeof:  js 世界 -> ts世界
+// 2. as const 字面量类型
+let str = '123'
+type s = typeof str
+str = '456'
+
+const cstr = '456'
+type c = typeof cstr
+// c = '789'
