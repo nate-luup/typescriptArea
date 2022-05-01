e3b8740532fa15103b1196b06eed146efac8f9fc
nate <nate.luup@gmail.com>
2022-05-01 10:26:44 +0800

    feat(00007): easy readonly

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -19,5 +19,5 @@ function mypick(todo, keys) {
 // 3. todo[key]取值
 //      https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html#handbook-content
 // 4. 判断key在不在todo里面
-//      typeof lookup: https://www.typescriptlang.org/docs/handbook/2/keyof-types.html#the-keyof-type-operator
+//      typeof lookup: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-1.html#keyof-and-lookup-types
 //      extends类型约束: https://www.typescriptlang.org/docs/handbook/2/generics.html#generic-constraints
--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,17 @@
-type MyReadonly<T> = any
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P]
+}
+
+// js
+function readonly(obj) {
+  const result = {}
+  for (let key in obj) {
+    result['readonly' + key] = obj[key]
+  }
+  return result
+}
+
+// 1. 返回一个对象
+// 2. 遍历 obj in--> mapped keyof lookup
+// 3. 加上readonly 关键字
+// 4. 通过key 来取obj里面的值 indexed
