83aa7e2232c6b9877d429743f43010d368c37753
nate <nate.luup@gmail.com>
2022-05-01 09:37:51 +0800

    feat(00004): complete easy pick

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,23 @@
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
+
+// js对比学习
+function mypick(todo, keys) {
+  let obj = {}
+  for (let key of keys) {
+    if (todo[key]) {
+      obj[key] = todo[key]
+    }
+  }
+  return obj
+}
+
+// 1. 返回一个对象
+// 2. 遍历keys
+//      https://www.typescriptlang.org/docs/handbook/2/mapped-types.html#handbook-content
+// 3. todo[key]取值
+//      https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html#handbook-content
+// 4. 判断key在不在todo里面
+//      typeof lookup: https://www.typescriptlang.org/docs/handbook/2/keyof-types.html#the-keyof-type-operator
+//      extends类型约束: https://www.typescriptlang.org/docs/handbook/2/generics.html#generic-constraints
