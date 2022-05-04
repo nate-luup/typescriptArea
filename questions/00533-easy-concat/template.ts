c35a7c1f568ea0e86f472372a07884d3dd9cab69
nate <nate.luup@gmail.com>
2022-05-04 21:29:25 +0800

    feat(000533): easy concat

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,1 +1,8 @@
-type Concat<T, U> = any
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U]
+
+// js实现
+function concat(arr1, arr2) {
+  let res = [...arr1, ...arr2]
+
+  return res
+}
