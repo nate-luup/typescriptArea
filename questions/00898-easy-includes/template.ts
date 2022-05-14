533e097cee01b94313c825d5b4a165689161c25f
nate <nate.luup@gmail.com>
2022-05-14 19:13:41 +0800

    feat(00898): easy includes

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,28 @@
-type Includes<T extends readonly any[], U> = any
+import type { Equal } from '@type-challenges/utils'
+
+export type Includes<T extends readonly any[], U> = T extends [
+  infer K,
+  ...infer P
+]
+  ? Equal<K, U> extends true
+    ? true
+    : Includes<P, U>
+  : false
+
+function include(list, key) {
+  let [first, ...rest] = list
+  if (list.length === 0) {
+    return false
+  }
+  if (first === key) {
+    return true
+  } else {
+    include(rest, key)
+  }
+}
+
+// 知识点
+// 1. 用递归实现遍历数组
+// 2. ts的模块规范
+//  - 如果有import/export的话就是模块
+//  - 没有的话就是全局的，可以直接在别的模块引用
